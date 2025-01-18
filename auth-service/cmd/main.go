90925248415e5270afdd5bf22fbed269d7f727df
VolodymyrShabat <volodymyrshabat@gmail.com>
2025-01-18 23:02:18 +0200

    Initial commit

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -18,22 +18,18 @@ type authServer struct {
 
 func main() {
 
-	//can use as flag also
 	cfg, err := config.LoadConfig("./config")
 	if err != nil {
 		log.Fatalf("Could not load config: %v", err)
 	}
 
-	// 2. Start a TCP listener on the configured port
 	address := fmt.Sprintf(":%d", cfg.Server.Port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("Failed to listen on port %d: %v", cfg.Server.Port, err)
 	}
 
-	// 3. Create a new gRPC server
 	grpcServer := grpc.NewServer()
-	// Register your gRPC services here, e.g.:
 	pb.RegisterAuthServiceServer(grpcServer, &handlers.AuthHandler{})
 
 	log.Printf("Auth Service listening on port %d...", cfg.Server.Port)
